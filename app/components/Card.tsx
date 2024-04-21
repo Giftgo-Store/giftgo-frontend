4f6ba6b9e0ae6d625aff3c3522193d95b50a00a4
Desmond-Fon <desmondfon@yahoo.com>
2024-04-21 08:49:46 +0100

    completed landing page

---
+++ app/components/Card.tsx
@@ -0,0 +1,70 @@
+"use client"
+import Image from "next/image";
+import { useState } from "react";
+import { FaStar } from "react-icons/fa6";
+import { IoEyeOutline } from "react-icons/io5";
+import { PiShoppingCart } from "react-icons/pi";
+import { LiaFighterJetSolid } from "react-icons/lia";
+
+type CardProps = {
+    express? : boolean
+}
+
+const Card = ({express= false} : CardProps) => {
+      const [isHovered, setIsHovered] = useState(false);
+
+      const handleMouseEnter = () => setIsHovered(true);
+      const handleMouseLeave = () => setIsHovered(false);
+    return (
+      <>
+        <div className="w-[290px] flex flex-col gap-[16px] border-[#E4E7E9] border-[1px] rounded-[8px] p-[8px] drop-shadow-sm hover:shadow-2xl shadow-white/12 cursor-pointer relative">
+           {express && <div className="bg-primary rounded-br-[8px] rounded-tl-[8px] absolute top-0 left-0 py-[7px] px-[10px] flex justify-center items-center gap-1 text-white z-50">
+              <LiaFighterJetSolid />
+              <p className="text-[11px]">Express shipping</p>
+            </div>}
+          <div
+            className="relative"
+            onMouseEnter={handleMouseEnter}
+            onMouseLeave={handleMouseLeave}
+          >
+            <Image
+              src="/jacket.png"
+              alt=""
+              width={276}
+              height={256}
+              className="relative z-0"
+            />
+            {isHovered && (
+              <div className="w-full h-full bg-cardBg z-40 absolute top-0 flex justify-center items-center">
+                <div className="flex justify-center items-center gap-2">
+                  <div className="w-[48px] h-[48px] rounded-full bg-primary flex justify-center items-center">
+                    <PiShoppingCart className="w-[24px] h-[24px] text-white" />
+                  </div>
+                  <div className="w-[48px] h-[48px] rounded-full bg-white flex justify-center items-center">
+                    <IoEyeOutline className="w-[24px] h-[24px]" />
+                  </div>
+                </div>
+              </div>
+            )}
+          </div>
+          <div className="flex flex-col items-start">
+            <p className="text-[14px] font-[500] text-[#475156] pb-1">
+              2-Barrel Carburetor Carb 2100 Engine Increase Horsepower
+            </p>
+
+            <div className="flex justify-start items-center gap-[2px]">
+              <FaStar className="text-[#FA8232] w-[13px] h-[12px]" />
+              <FaStar className="text-[#FA8232] w-[13px] h-[12px]" />
+              <FaStar className="text-[#FA8232] w-[13px] h-[12px]" />
+              <FaStar className="text-[#FA8232] w-[13px] h-[12px]" />
+              <FaStar className="text-[#FA8232] w-[13px] h-[12px]" />
+              <p className="text-[#77878F] text-[12px] pl-1">(583)</p>
+            </div>
+            <p className="text-[#05031A] text-[16px] font-[600] pt-2">₦160</p>
+          </div>
+        </div>
+      </>
+    );
+}
+ 
+export default Card;
---
+++ app/components/LandingHeader.tsx
@@ -0,0 +1,80 @@
+import Image from "next/image";
+import { FiSearch } from "react-icons/fi";
+import { PiMapPinLine, PiShoppingCart } from "react-icons/pi";
+import { FiHeadphones, FiPhoneCall } from "react-icons/fi";
+import { AiOutlineUser } from "react-icons/ai";
+
+const LandingHeader = () => {
+    return (
+      <>
+        <div className="py-3 flex justify-between w-full items-center bg-secondary px-[8%]">
+          <p className="text-[14px] leading-[20px]">
+            Welcome to Giftgo online store.{" "}
+          </p>
+          <div className="flex justify-center items-center gap-[24px] text-[14px] font-[500]">
+            <select name="" id="" className="border-none bg-secondary outline-none">
+              <option value="">Eng</option>
+            </select>
+            <select name="" id="" className="border-none bg-secondary outline-none">
+              <option value="">NGN</option>
+            </select>
+          </div>
+        </div>
+
+        <div className="px-[8%] flex flex-col justify-between items-center bg-primary pt-[20px] pb-[16px] gap-[20px]">
+          <div className="flex justify-between items-center w-full">
+            <Image src="/icon.svg" alt="" width={118} height={48} />
+            <div className="relative">
+              <input
+                type="search"
+                className="rounded-[4px] h-[48px] w-[600px] px-[20px] py-[14px] outline-none text-[14px]"
+                placeholder="Search for anything..."
+              />
+              <FiSearch className="absolute right-4 top-4 w-[20px] h-[20px]" />
+            </div>
+            <div className="flex justify-center items-center gap-[24px]">
+              <div className="flex justify-center items-center gap-1 text-[14px] text-white leading-[20px]">
+                <PiMapPinLine />
+                <p>Track Order</p>
+              </div>
+              <div className="flex justify-center items-center gap-1 text-[14px] text-white leading-[20px]">
+                <FiHeadphones />
+                <p>Customer SUpport</p>
+              </div>
+            </div>
+          </div>
+
+          <div className="flex justify-between items-center w-full">
+            <select
+              name=""
+              id=""
+              className="rounded-[4px] h-[48px] w-[150px] text-[14px] font-[600] px-[10px] py-[14px] outline-none text-black"
+            >
+              <option value="All category">All category</option>
+            </select>
+
+            <div className="flex justify-center items-center gap-[32px]">
+              <div className="flex justify-center items-center gap-2 text-[18px] text-white leading-[20px]">
+                <FiPhoneCall />
+                <p>+234 9000000000</p>
+              </div>
+              <PiShoppingCart className="w-[25px] text-white h-[25px]" />
+              <AiOutlineUser className="w-[25px] text-white h-[25px]" />
+              <select
+                name=""
+                id=""
+                className="border-none bg-primary text-white outline-none text-[16px] font-[500]"
+              >
+                <option value="" className="flex justify-center gap-1 items-center">
+                  Ship to{" "}
+                  <Image src="/flag.png" alt="" width={20} height={10} />
+                </option>
+              </select>
+            </div>
+          </div>
+        </div>
+      </>
+    );
+}
+ 
+export default LandingHeader;
