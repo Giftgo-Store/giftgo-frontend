b679de99cc4820eae92a72c79d837960ea7ff44c
“ShedrachJonah11” <ShedrachJonah11@gmail.com>
2024-05-24 02:50:59 +0100

    feat: add loading component

--- app/components/LoginModal.tsx
+++ app/components/LoginModal.tsx
@@ -159,8 +159,33 @@ const Modal: React.FC<ModalProps> = ({ showModal, closeModal }) => {
                 type="submit"
                 className="w-full bg-primary text-white py-2 h-[48px] rounded-[2px] flex justify-center items-center gap-4 font-[700]"
               >
-                SIGN IN
-                <FaArrowRight />
+                {isSigningIn ? (
+                  <svg
+                    className="animate-spin h-5 w-5 mr-3 text-white"
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                  >
+                    <circle
+                      className="opacity-25"
+                      cx="12"
+                      cy="12"
+                      r="10"
+                      stroke="currentColor"
+                      strokeWidth="4"
+                    ></circle>
+                    <path
+                      className="opacity-75"
+                      fill="currentColor"
+                      d="M4 12a8 8 0 018-8v8H4z"
+                    ></path>
+                  </svg>
+                ) : (
+                  <>
+                    SIGN IN
+                    <FaArrowRight />
+                  </>
+                )}
               </button>
             </form>
             <div className="flex justify-between px-6 items-center">
