bd17b89b49c7f30c3b8480cf2e1d10d4bcd39df9
Desmond-Fon <desmondfon@yahoo.com>
2024-08-08 12:25:58 +0100

    chore: fixing production error

--- app/components/cards/Card.tsx
+++ app/components/cards/Card.tsx
@@ -77,7 +77,7 @@ const Card = ({ lists }: CardProps) => {
         images={lists?.images}
       />
       <div className="w-[290px] flex flex-col gap-[16px] border-[#E4E7E9] border-[1px] rounded-[8px] p-[8px] drop-shadow-sm hover:shadow-2xl shadow-white/12 cursor-pointer relative">
-        {lists && lists?.expressShipping && (
+        {lists && lists?.expressShipping === 'true' && (
           <div className="bg-primary rounded-br-[8px] rounded-tl-[8px] absolute top-0 left-0 py-[7px] px-[10px] flex justify-center items-center gap-1 text-white z-50">
             <LiaFighterJetSolid />
             <p className="text-[11px]">Express shipping</p>
