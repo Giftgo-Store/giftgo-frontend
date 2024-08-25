1fd089f94996e79fa4acb138d4325dbfd17d6326
Ephraimiyanda <iyandaephraim@gmail.com>
2024-08-25 22:34:30 +0100

    feature: added search feature for reviews

--- app/(pages)/admin/reviews/[id]/page.tsx
+++ app/(pages)/admin/reviews/[id]/page.tsx
@@ -5,7 +5,8 @@ import BASE_URL from "@/app/config/baseurl";
 import { useEffect, useState } from "react";
 import ReviewCard from "@/app/components/review/reviewCard";
 import { toast } from "react-toastify";
-import { Spinner } from "@nextui-org/react";
+import { Input, Spinner } from "@nextui-org/react";
+import { CiSearch } from "react-icons/ci";
 interface review {
   reviewerName: string;
   rating: number;
@@ -16,6 +17,7 @@ interface review {
 export default function Reviews({ params }: { params: { id: string } }) {
   const [loading, setLoading] = useState(true);
   const [reviews, setReviews] = useState([]);
+  const [filterValue, setFilterValue] = useState("");
   const id = params.id;
   const sesssion = useSession({
     required: true,
@@ -74,33 +76,65 @@ export default function Reviews({ params }: { params: { id: string } }) {
     fetchReviews();
   }, []);
 
+ // Filter categories based on filterValue
+  const filteredReviews = reviews?.filter(
+    (review: review) =>
+      review.reviewerName.toLowerCase().includes(filterValue.toLowerCase()) ||
+      review.comment.toLowerCase().includes(filterValue.toLowerCase())
+  );
   return (
-    <div className="flex flex-wrap w-full justify-between ">
-      {reviews.map((review: review) => (
-        <ReviewCard
-          DeleteReview={() => {
-            toast.promise(DeleteReview(review._id), {
-              pending: "Deleting review ",
-              success: "Review deleted ",
-              error: "An error occured , please try again",
-            });
+    <div>
+      <div className="flex w-full justify-between items-center gap-4">
+        <Input
+          placeholder="Search using reviewer name or keyword"
+          endContent={<CiSearch size={28} color="#8B909A" />}
+          size="md"
+          radius="sm"
+          aria-label="search"
+          className="max-w-[350px] w-full shadow-none"
+          classNames={{
+            label: "text-lg",
+            input: "py-2 text-base",
+            inputWrapper: [
+              "bg-white",
+              "data-focus-[within=true]:bg-white",
+              "data-[hover=true]:bg-white",
+              "group-data-[focus=true]:bg-white",
+            ],
           }}
-          reviewRating={review.rating}
-          comment={review.comment}
-          userName={review.reviewerName}
-          timestamp={review.createdAt}
-        />
-      ))}
-      {reviews.length < 1 && !loading && (
-        <div className="min-h-[40vh] flex justify-center items-center w-full">
-          <p className="text-lg font-semibold ">No reviews found !</p>
-        </div>
-      )}
-      {reviews.length < 1 && loading && (
-        <div className="min-h-[40vh] w-full flex justify-center items-center mx-auto">
-          <Spinner color="default" className="mx-auto"></Spinner>
-        </div>
-      )}
+          value={filterValue}
+          onChange={(e) => {
+            setFilterValue(e.target.value);
+          }}
+        ></Input>
+      </div>
+      <div className="flex flex-wrap w-full justify-between ">
+        {filteredReviews.map((review: review) => (
+          <ReviewCard
+            DeleteReview={() => {
+              toast.promise(DeleteReview(review._id), {
+                pending: "Deleting review ",
+                success: "Review deleted ",
+                error: "An error occured , please try again",
+              });
+            }}
+            reviewRating={review.rating}
+            comment={review.comment}
+            userName={review.reviewerName}
+            timestamp={review.createdAt}
+          />
+        ))}
+        {filteredReviews.length < 1 && !loading && (
+          <div className="min-h-[40vh] flex justify-center items-center w-full">
+            <p className="text-lg font-semibold ">No reviews found !</p>
+          </div>
+        )}
+        {filteredReviews.length < 1 && loading && (
+          <div className="min-h-[40vh] w-full flex justify-center items-center mx-auto">
+            <Spinner color="default" className="mx-auto"></Spinner>
+          </div>
+        )}
+      </div>
     </div>
   );
 }
